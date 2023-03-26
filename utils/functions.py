a35fe34d3473e86ee4818295caa36f480fca1a94
Umoh Nsikakabasi E <91345422+NsikakabasiUmoh@users.noreply.github.com>
2023-03-26 00:54:23 -0400

    Added Working Conversion for F and K

--- utils/functions.py
+++ utils/functions.py
@@ -14,8 +14,22 @@ def convertor(value:float, param1, param2) -> float:
     else:
         if param1 == 'C':
             if param2 == 'F':
-                return 1.8 * value + 32
+                return f'{1.8 * value + 32} F'
             elif param2 == 'K':
-                return value + 273.15
+                return f'{value + 273.15} K'
             else:
-                return value
+                return f'{value} C'
+        elif param1 == 'F':
+            if param2 == 'C':
+                return f'{0.55 * (value - 32)} C'
+            elif param2 == 'K':
+                return f'{(0.55 * (value - 32)) + 273.15} K'
+            else:
+                return f'{value} F'
+        elif param1 == 'K':
+            if param2 == 'C':
+                return f'{value - 273.15} C'
+            elif param2 == 'F':
+                return f'{1.8 * (value - 273.15) + 32} F'
+            else:
+                return f'{value} K'
